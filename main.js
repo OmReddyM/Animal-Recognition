361f00dea17258802d554f0f2055c794fc0a458c
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2021-12-24 22:08:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,5 +12,23 @@ function resultFunc(error, results) {
         console.error(error);
     } else {
         console.log(results);
+        R = Math.floor(Math.random() * 255) + 1;
+        G = Math.floor(Math.random() * 255) + 1;
+        B = Math.floor(Math.random() * 255) + 1;
+        RGB = "rgb(" + R + ", " + G + ", " + B + ")";
+        Accuracy = (results[0].confidence * 100).toFixed(2) + "%";
+        topResult = results[0].label;
+
+        document.getElementById("resultLabel").innerHTML = "I can hear - " + topResult;
+        document.getElementById("accuracyLabel").innerHTML = Accuracy;
+        document.getElementById("resultLabel").style.color = RGB;
+        document.getElementById("accuracyLabel").style.color = RGB;
+
+        resultImage = document.getElementById("resultImage");
+        if (topResult == "Barking") {
+            resultImage.src = "barking.gif";
+        } else if (topResult == "Meowing") {
+            resultImage.src == "meowing.gif";
+        }
     }
 }
