9b35f566c6103698eea6a154ca4e5d7d03faef6e
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2021-12-24 21:59:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 "https://teachablemachine.withgoogle.com/models/dG3FzD1dE/"
+function startClassification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    audioClassifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/dG3FzD1dE/model.json", modelLoaded);
+}
+
+function modelLoaded() {
+    audioClassifier.classify(resultFunc);
+}
+
+function resultFunc(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
+}
